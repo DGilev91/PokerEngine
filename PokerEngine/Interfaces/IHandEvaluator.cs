ba5f688e989ca2b3edce516f98623c0570d5c28d
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2026-08-01 18:59:34 +0500

    HandRank

---
+++ PokerEngine/Interfaces/IHandEvaluator.cs
@@ -0,0 +1,8 @@
+﻿using PokerEngine.Models;
+
+namespace PokerEngine.Interfaces;
+
+public interface IHandEvaluator
+{
+    HandRank Evaluate(IReadOnlyList<string> holeCards, IReadOnlyList<string> boardCards);
+}
---
+++ PokerEngine/Models/HandRank.cs
@@ -0,0 +1,41 @@
+﻿using PokerEngine.Enums;
+
+namespace PokerEngine.Models;
+
+public sealed class HandRank
+{
+    public HandCategory Category { get; }
+
+    public long Strength { get; }
+
+    public IReadOnlyList<string> Cards { get; }
+
+    public HandRank(
+        HandCategory category,
+        long strength,
+        IReadOnlyList<string> cards)
+    {
+        ArgumentNullException.ThrowIfNull(cards);
+
+        if (cards.Count != 5)
+        {
+            throw new ArgumentException(
+                "Покерная комбинация должна состоять из пяти карт.",
+                nameof(cards));
+        }
+
+        Category = category;
+        Strength = strength;
+        Cards = cards.ToArray();
+    }
+
+    public int CompareTo(HandRank? other)
+    {
+        if (other is null)
+        {
+            return 1;
+        }
+
+        return Strength.CompareTo(other.Strength);
+    }
+}
