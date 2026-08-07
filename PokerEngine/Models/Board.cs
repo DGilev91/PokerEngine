7da3371fb78b87fc05614985abfa5f03d9e3e2d6
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2026-08-07 19:03:08 +0500

    board

--- PokerEngine/Models/Board.cs
+++ PokerEngine/Models/Board.cs
@@ -2,16 +2,9 @@
 
 public sealed class Board
 {
-    public int Index { get; }
-
     public IReadOnlyList<string> Cards => _cards;
 
     private readonly List<string> _cards = [];
-
-    internal Board(int index)
-    {
-        Index = index;
-    }
 
     internal void Deal(List<string> cards)
     {
